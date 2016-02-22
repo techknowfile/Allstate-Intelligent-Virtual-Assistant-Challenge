f1a12e236dd719a37fbfeaa9b35f96052a7afb48
Andy <andy@nau.edu>
2016-02-22 08:37:15 -0700

    Added basic functionality for determining KB article and printing out the resolution steps

--- chatClient.py
+++ chatClient.py
@@ -6,13 +6,14 @@ from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 
-issueDetermined = False
-issueResolved = False
 kbArticleID = -1
 
 # CONSTANTS
 BOTNAME = "Alice (TSR)"
-USERNAME = "User"
+DEFAULT_USERNAME = "User"
+
+# User data
+username = ''
 
 # NLTK Objects
 lemmatizer = WordNetLemmatizer()
@@ -26,24 +27,23 @@ stop_words.remove('no')
 #################################################
 class KBArticle:
 	def __init__(self, resolution):
-		self.resolution = resolution;
+		self.resolution = resolution; # Tuple of resolution steps
 
 kbKeywordDict = {'address':(9, 'KB00206580'), 'login':(7, 'KB0083060')}
 
-kb1 = KBArticle('''1. Resolve the incident by unlocking the email account.
-2. Verify with the user that their issue has been resolved.
-3. Help the user understand and correct the root cause if necessary.
-   a. If the user has a mobile device connected to their work email, verify that the user has updated the password on their mobile device as well, since that may be the root cause of the account becoming locked.
-''')
+kb1 = KBArticle(('Resolve the incident by unlocking the email account.',
+'Verify with the user that their issue has been resolved.',
+'''Help the user understand and correct the root cause if necessary.
+   a. If the user has a mobile device connected to their work email, verify that the user has updated the password on their mobile device as well, since that may be the root cause of the account becoming locked.'''
+   ))
 
-kb2 = KBArticle('''Resolution/Workaround
-1. Verify the customer’s current billing address
-2. Verify the customer’s current mailing address
-3. Verify that the customer is requesting a change to their billing address only or also their mailing address
-4. Update their billing address
-5. If the customer would like to update their mailing address as well, update their mailing address
-6. Confirm the customer’s new billing address and, if applicable, their mailing address after the change'''
-)
+kb2 = KBArticle(('Verify the customer\'s current billing address',
+				'Verify the customer\'s current mailing address',
+				'Verify that the customer is requesting a change to their billing address only or also their mailing address',
+				'Update their billing address',
+				'If the customer would like to update their mailing address as well, update their mailing address',
+				'Confirm the customer\'s new billing address and, if applicable, their mailing address after the change'
+))
 
 kbLibrary = {'KB0083060':kb1, 'KB00206580':kb2}
 
@@ -52,16 +52,31 @@ kbLibrary = {'KB0083060':kb1, 'KB00206580':kb2}
 ##############################################
 def main():
 	''' TODO: Docstring'''
+	issueResolved = False
 	clearScreen();
+	getUsername();
+	kbArticle = None
 
 	while not issueResolved:
-		kbArticle = determineIssue() #Gets Key
-		print(kbArticle)
+		kwArticlePair = determineIssue() #Gets Key
+		if kwArticlePair:
+			# get the Article from the Knowledge Base Library
+			kbArticle = kbLibrary.get(kwArticlePair[1])
+			for step in kbArticle.resolution:
+				tellUser(step)
 
-	tellUser("Is there anything else I can help you with today?")
+
+			# issue resolved
+			tellUser("Is there anything else I can help you with today?")
+
+			# Check if user says no, yes, or gives another problem
+
+			# If no, give final statement and end chat
+
+	
 
 def determineIssue():
-	tellUser("How can I help you?")
+	tellUser("Hello, {}! How can I help you today? ".format(username))
 	userInput = tellBot() # Get user input
 	keywords = parseInput(userInput) # parse keywords from user input
 	
@@ -73,9 +88,6 @@ def determineIssue():
 		issueDetermined = True
 		# Returns only the highest rated match. Remove [0] to return all matched Keyword/KB article pairs
 		return matched_keywords[0][1]
-
-	if issueDetermined:
-		pass
 	
 	else:
 		tellUser("I'm sorry, I'm not able to assist with that issue.")
@@ -85,7 +97,7 @@ def tellUser(response):
 	print(BOTNAME + ":", response)
 
 def tellBot():
-	return input(USERNAME + ": ")
+	return input(username + ": ")
 
 def clearScreen():
 	''' Function to clear current screen based on current OS '''
@@ -97,6 +109,15 @@ def parseInput(userInput):
 	# Remove stop words and words less than 2 characters long, then LEMMATIZE input
 	#words = [lemmatizer.lemmatize(w) for w in words if not w in stop_words and len(w) > 1] 
 	return words
+
+def getUsername():
+	global username
+	username = input('''Welcome to TeamDudley\'s Virtual Assistant!
+
+To get started, simply enter your name: ''')
+	if username == '':
+		username = DEFAULT_USERNAME
+	clearScreen();
 
 
 # END OF DOCUMENT
